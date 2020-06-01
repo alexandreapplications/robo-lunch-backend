d8e1f4ff566c1d6e8a39397c73e2a3c797920807
Alexandre Sampaio <alexandrenomsn@hotmail.com>
2020-06-01 11:30:04 -0300

    Add swagger draft

--- app/app.js
+++ app/app.js
@@ -8,4 +8,10 @@ var itemRouter = require("./routes/itemRouter");
 router.get("/alive", (req, res) => res.send("The application is alive."));
 router.use("/item", itemRouter);
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./app/swagger.yaml");
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 module.exports = router;
